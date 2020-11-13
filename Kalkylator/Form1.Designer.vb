ba686858db6f9bc57553d131d563f70d61c5e2ae
moggesmith10 <moggelitojonatan22@gmail.com>
2020-11-13 08:52:25 +0200

    Initial commit

---
+++ Kalkylator/Form1.Designer.vb
@@ -0,0 +1,374 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+		Me.btnSquare = New System.Windows.Forms.Button()
+		Me.tbxMain = New System.Windows.Forms.TextBox()
+		Me.btnDivideByX = New System.Windows.Forms.Button()
+		Me.btnSquareRoot = New System.Windows.Forms.Button()
+		Me.btnDivision = New System.Windows.Forms.Button()
+		Me.btnMemClear = New System.Windows.Forms.Button()
+		Me.btnNum7 = New System.Windows.Forms.Button()
+		Me.btnNum8 = New System.Windows.Forms.Button()
+		Me.btnNum9 = New System.Windows.Forms.Button()
+		Me.btnNum6 = New System.Windows.Forms.Button()
+		Me.btnNum5 = New System.Windows.Forms.Button()
+		Me.btnNum4 = New System.Windows.Forms.Button()
+		Me.btnNum3 = New System.Windows.Forms.Button()
+		Me.btnNum2 = New System.Windows.Forms.Button()
+		Me.btnNum1 = New System.Windows.Forms.Button()
+		Me.btnMultiply = New System.Windows.Forms.Button()
+		Me.btnSubtract = New System.Windows.Forms.Button()
+		Me.btnPlus = New System.Windows.Forms.Button()
+		Me.btnNum0 = New System.Windows.Forms.Button()
+		Me.btnComma = New System.Windows.Forms.Button()
+		Me.btnFlip = New System.Windows.Forms.Button()
+		Me.btnEqual = New System.Windows.Forms.Button()
+		Me.btnMemAdd = New System.Windows.Forms.Button()
+		Me.btnMemReturn = New System.Windows.Forms.Button()
+		Me.btnClear = New System.Windows.Forms.Button()
+		Me.btnAllClear = New System.Windows.Forms.Button()
+		Me.SuspendLayout()
+		'
+		'btnSquare
+		'
+		Me.btnSquare.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnSquare.Location = New System.Drawing.Point(12, 38)
+		Me.btnSquare.Name = "btnSquare"
+		Me.btnSquare.Size = New System.Drawing.Size(35, 35)
+		Me.btnSquare.TabIndex = 0
+		Me.btnSquare.Text = "X²"
+		Me.btnSquare.UseVisualStyleBackColor = True
+		'
+		'tbxMain
+		'
+		Me.tbxMain.Location = New System.Drawing.Point(12, 12)
+		Me.tbxMain.Name = "tbxMain"
+		Me.tbxMain.Size = New System.Drawing.Size(199, 20)
+		Me.tbxMain.TabIndex = 1
+		'
+		'btnDivideByX
+		'
+		Me.btnDivideByX.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnDivideByX.Location = New System.Drawing.Point(53, 38)
+		Me.btnDivideByX.Name = "btnDivideByX"
+		Me.btnDivideByX.Size = New System.Drawing.Size(35, 35)
+		Me.btnDivideByX.TabIndex = 2
+		Me.btnDivideByX.Text = "1/X"
+		Me.btnDivideByX.UseVisualStyleBackColor = True
+		'
+		'btnSquareRoot
+		'
+		Me.btnSquareRoot.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnSquareRoot.Location = New System.Drawing.Point(94, 38)
+		Me.btnSquareRoot.Name = "btnSquareRoot"
+		Me.btnSquareRoot.Size = New System.Drawing.Size(35, 35)
+		Me.btnSquareRoot.TabIndex = 3
+		Me.btnSquareRoot.Text = "√"
+		Me.btnSquareRoot.UseVisualStyleBackColor = True
+		'
+		'btnDivision
+		'
+		Me.btnDivision.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnDivision.Location = New System.Drawing.Point(135, 38)
+		Me.btnDivision.Name = "btnDivision"
+		Me.btnDivision.Size = New System.Drawing.Size(35, 35)
+		Me.btnDivision.TabIndex = 4
+		Me.btnDivision.Text = "/"
+		Me.btnDivision.UseVisualStyleBackColor = True
+		'
+		'btnMemClear
+		'
+		Me.btnMemClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnMemClear.Location = New System.Drawing.Point(176, 38)
+		Me.btnMemClear.Name = "btnMemClear"
+		Me.btnMemClear.Size = New System.Drawing.Size(35, 35)
+		Me.btnMemClear.TabIndex = 5
+		Me.btnMemClear.Text = "MC"
+		Me.btnMemClear.UseVisualStyleBackColor = True
+		'
+		'btnNum7
+		'
+		Me.btnNum7.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnNum7.Location = New System.Drawing.Point(12, 79)
+		Me.btnNum7.Name = "btnNum7"
+		Me.btnNum7.Size = New System.Drawing.Size(35, 35)
+		Me.btnNum7.TabIndex = 6
+		Me.btnNum7.Text = "7"
+		Me.btnNum7.UseVisualStyleBackColor = True
+		'
+		'btnNum8
+		'
+		Me.btnNum8.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnNum8.Location = New System.Drawing.Point(53, 79)
+		Me.btnNum8.Name = "btnNum8"
+		Me.btnNum8.Size = New System.Drawing.Size(35, 35)
+		Me.btnNum8.TabIndex = 7
+		Me.btnNum8.Text = "8"
+		Me.btnNum8.UseVisualStyleBackColor = True
+		'
+		'btnNum9
+		'
+		Me.btnNum9.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnNum9.Location = New System.Drawing.Point(94, 79)
+		Me.btnNum9.Name = "btnNum9"
+		Me.btnNum9.Size = New System.Drawing.Size(35, 35)
+		Me.btnNum9.TabIndex = 8
+		Me.btnNum9.Text = "9"
+		Me.btnNum9.UseVisualStyleBackColor = True
+		'
+		'btnNum6
+		'
+		Me.btnNum6.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnNum6.Location = New System.Drawing.Point(94, 120)
+		Me.btnNum6.Name = "btnNum6"
+		Me.btnNum6.Size = New System.Drawing.Size(35, 35)
+		Me.btnNum6.TabIndex = 11
+		Me.btnNum6.Text = "6"
+		Me.btnNum6.UseVisualStyleBackColor = True
+		'
+		'btnNum5
+		'
+		Me.btnNum5.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnNum5.Location = New System.Drawing.Point(53, 120)
+		Me.btnNum5.Name = "btnNum5"
+		Me.btnNum5.Size = New System.Drawing.Size(35, 35)
+		Me.btnNum5.TabIndex = 10
+		Me.btnNum5.Text = "5"
+		Me.btnNum5.UseVisualStyleBackColor = True
+		'
+		'btnNum4
+		'
+		Me.btnNum4.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnNum4.Location = New System.Drawing.Point(12, 120)
+		Me.btnNum4.Name = "btnNum4"
+		Me.btnNum4.Size = New System.Drawing.Size(35, 35)
+		Me.btnNum4.TabIndex = 9
+		Me.btnNum4.Text = "4"
+		Me.btnNum4.UseVisualStyleBackColor = True
+		'
+		'btnNum3
+		'
+		Me.btnNum3.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnNum3.Location = New System.Drawing.Point(94, 161)
+		Me.btnNum3.Name = "btnNum3"
+		Me.btnNum3.Size = New System.Drawing.Size(35, 35)
+		Me.btnNum3.TabIndex = 14
+		Me.btnNum3.Text = "3"
+		Me.btnNum3.UseVisualStyleBackColor = True
+		'
+		'btnNum2
+		'
+		Me.btnNum2.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnNum2.Location = New System.Drawing.Point(53, 161)
+		Me.btnNum2.Name = "btnNum2"
+		Me.btnNum2.Size = New System.Drawing.Size(35, 35)
+		Me.btnNum2.TabIndex = 13
+		Me.btnNum2.Text = "2"
+		Me.btnNum2.UseVisualStyleBackColor = True
+		'
+		'btnNum1
+		'
+		Me.btnNum1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnNum1.Location = New System.Drawing.Point(12, 161)
+		Me.btnNum1.Name = "btnNum1"
+		Me.btnNum1.Size = New System.Drawing.Size(35, 35)
+		Me.btnNum1.TabIndex = 12
+		Me.btnNum1.Text = "1"
+		Me.btnNum1.UseVisualStyleBackColor = True
+		'
+		'btnMultiply
+		'
+		Me.btnMultiply.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnMultiply.Location = New System.Drawing.Point(135, 79)
+		Me.btnMultiply.Name = "btnMultiply"
+		Me.btnMultiply.Size = New System.Drawing.Size(35, 35)
+		Me.btnMultiply.TabIndex = 15
+		Me.btnMultiply.Text = "X"
+		Me.btnMultiply.UseVisualStyleBackColor = True
+		'
+		'btnSubtract
+		'
+		Me.btnSubtract.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnSubtract.Location = New System.Drawing.Point(135, 120)
+		Me.btnSubtract.Name = "btnSubtract"
+		Me.btnSubtract.Size = New System.Drawing.Size(35, 35)
+		Me.btnSubtract.TabIndex = 16
+		Me.btnSubtract.Text = "-"
+		Me.btnSubtract.UseVisualStyleBackColor = True
+		'
+		'btnPlus
+		'
+		Me.btnPlus.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnPlus.Location = New System.Drawing.Point(135, 161)
+		Me.btnPlus.Name = "btnPlus"
+		Me.btnPlus.Size = New System.Drawing.Size(35, 35)
+		Me.btnPlus.TabIndex = 17
+		Me.btnPlus.Text = "+"
+		Me.btnPlus.UseVisualStyleBackColor = True
+		'
+		'btnNum0
+		'
+		Me.btnNum0.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnNum0.Location = New System.Drawing.Point(12, 202)
+		Me.btnNum0.Name = "btnNum0"
+		Me.btnNum0.Size = New System.Drawing.Size(35, 35)
+		Me.btnNum0.TabIndex = 18
+		Me.btnNum0.Text = "0"
+		Me.btnNum0.UseVisualStyleBackColor = True
+		'
+		'btnComma
+		'
+		Me.btnComma.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnComma.Location = New System.Drawing.Point(53, 202)
+		Me.btnComma.Name = "btnComma"
+		Me.btnComma.Size = New System.Drawing.Size(35, 35)
+		Me.btnComma.TabIndex = 19
+		Me.btnComma.Text = "."
+		Me.btnComma.UseVisualStyleBackColor = True
+		'
+		'btnFlip
+		'
+		Me.btnFlip.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnFlip.Location = New System.Drawing.Point(94, 202)
+		Me.btnFlip.Name = "btnFlip"
+		Me.btnFlip.Size = New System.Drawing.Size(35, 35)
+		Me.btnFlip.TabIndex = 20
+		Me.btnFlip.Text = "+/-"
+		Me.btnFlip.UseVisualStyleBackColor = True
+		'
+		'btnEqual
+		'
+		Me.btnEqual.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnEqual.Location = New System.Drawing.Point(135, 202)
+		Me.btnEqual.Name = "btnEqual"
+		Me.btnEqual.Size = New System.Drawing.Size(35, 35)
+		Me.btnEqual.TabIndex = 21
+		Me.btnEqual.Text = "="
+		Me.btnEqual.UseVisualStyleBackColor = True
+		'
+		'btnMemAdd
+		'
+		Me.btnMemAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnMemAdd.Location = New System.Drawing.Point(176, 79)
+		Me.btnMemAdd.Name = "btnMemAdd"
+		Me.btnMemAdd.Size = New System.Drawing.Size(35, 35)
+		Me.btnMemAdd.TabIndex = 22
+		Me.btnMemAdd.Text = "M+"
+		Me.btnMemAdd.UseVisualStyleBackColor = True
+		'
+		'btnMemReturn
+		'
+		Me.btnMemReturn.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnMemReturn.Location = New System.Drawing.Point(176, 120)
+		Me.btnMemReturn.Name = "btnMemReturn"
+		Me.btnMemReturn.Size = New System.Drawing.Size(35, 35)
+		Me.btnMemReturn.TabIndex = 23
+		Me.btnMemReturn.Text = "MR"
+		Me.btnMemReturn.UseVisualStyleBackColor = True
+		'
+		'btnClear
+		'
+		Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnClear.Location = New System.Drawing.Point(176, 161)
+		Me.btnClear.Name = "btnClear"
+		Me.btnClear.Size = New System.Drawing.Size(35, 35)
+		Me.btnClear.TabIndex = 24
+		Me.btnClear.Text = "C"
+		Me.btnClear.UseVisualStyleBackColor = True
+		'
+		'btnAllClear
+		'
+		Me.btnAllClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!)
+		Me.btnAllClear.Location = New System.Drawing.Point(176, 202)
+		Me.btnAllClear.Name = "btnAllClear"
+		Me.btnAllClear.Size = New System.Drawing.Size(35, 35)
+		Me.btnAllClear.TabIndex = 25
+		Me.btnAllClear.Text = "AC"
+		Me.btnAllClear.UseVisualStyleBackColor = True
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(217, 247)
+		Me.Controls.Add(Me.btnAllClear)
+		Me.Controls.Add(Me.btnClear)
+		Me.Controls.Add(Me.btnMemReturn)
+		Me.Controls.Add(Me.btnMemAdd)
+		Me.Controls.Add(Me.btnEqual)
+		Me.Controls.Add(Me.btnFlip)
+		Me.Controls.Add(Me.btnComma)
+		Me.Controls.Add(Me.btnNum0)
+		Me.Controls.Add(Me.btnPlus)
+		Me.Controls.Add(Me.btnSubtract)
+		Me.Controls.Add(Me.btnMultiply)
+		Me.Controls.Add(Me.btnNum3)
+		Me.Controls.Add(Me.btnNum2)
+		Me.Controls.Add(Me.btnNum1)
+		Me.Controls.Add(Me.btnNum6)
+		Me.Controls.Add(Me.btnNum5)
+		Me.Controls.Add(Me.btnNum4)
+		Me.Controls.Add(Me.btnNum9)
+		Me.Controls.Add(Me.btnNum8)
+		Me.Controls.Add(Me.btnNum7)
+		Me.Controls.Add(Me.btnMemClear)
+		Me.Controls.Add(Me.btnDivision)
+		Me.Controls.Add(Me.btnSquareRoot)
+		Me.Controls.Add(Me.btnDivideByX)
+		Me.Controls.Add(Me.tbxMain)
+		Me.Controls.Add(Me.btnSquare)
+		Me.Name = "Form1"
+		Me.Text = "Kalkylator"
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+
+	Friend WithEvents btnSquare As Button
+	Friend WithEvents tbxMain As TextBox
+	Friend WithEvents btnDivideByX As Button
+	Friend WithEvents btnSquareRoot As Button
+	Friend WithEvents btnDivision As Button
+	Friend WithEvents btnMemClear As Button
+	Friend WithEvents btnNum7 As Button
+	Friend WithEvents btnNum8 As Button
+	Friend WithEvents btnNum9 As Button
+	Friend WithEvents btnNum6 As Button
+	Friend WithEvents btnNum5 As Button
+	Friend WithEvents btnNum4 As Button
+	Friend WithEvents btnNum3 As Button
+	Friend WithEvents btnNum2 As Button
+	Friend WithEvents btnNum1 As Button
+	Friend WithEvents btnMultiply As Button
+	Friend WithEvents btnSubtract As Button
+	Friend WithEvents btnPlus As Button
+	Friend WithEvents btnNum0 As Button
+	Friend WithEvents btnComma As Button
+	Friend WithEvents btnFlip As Button
+	Friend WithEvents btnEqual As Button
+	Friend WithEvents btnMemAdd As Button
+	Friend WithEvents btnMemReturn As Button
+	Friend WithEvents btnClear As Button
+	Friend WithEvents btnAllClear As Button
+End Class
---
+++ Kalkylator/Form1.vb
@@ -0,0 +1,304 @@
+﻿Public Class Form1
+	Sub Calculate()
+		If (VerifyLegalMath()) Then
+			CalculateSquaresAndRoots()
+			CalculateMultiplicationAndDivison()
+			CalculateAdditionAndSubtraction()
+		End If
+	End Sub
+
+	Function VerifyLegalMath()
+		If (VerifyRootsLegal() And VerifyLastCharLegal()) Then 'Go through all checks
+			Return True 'If all legal return true
+		End If
+
+		Return False 'Else return false
+	End Function
+
+	Function VerifyRootsLegal()
+		Dim checkedRoots = 0 'Amount of string already checked, TODO check it doesnt lock up
+		While (tbxMain.Text.Substring(checkedRoots).Contains("√")) 'Loop through all roots, dont check any before index checkedRoots
+			checkedRoots = InStr(tbxMain.Text.Substring(checkedRoots), "√") 'Get next root
+			If (IsNumeric(tbxMain.Text(checkedRoots))) Then 'Make sure root is prepended to number
+				Return False ' return false if not
+			End If
+		End While
+		Return True 'If all roots are legal return true
+	End Function
+
+	Function VerifyLastCharLegal()
+		If (Not (IsNumeric(tbxMain.Text(tbxMain.Text.Length - 1)) Or tbxMain.Text(tbxMain.Text.Length - 1) = "²")) Then 'Last char can only be ² or numeric
+			Return False
+		End If
+		Return True
+	End Function
+
+	'calculate x² and √x
+	Sub CalculateSquaresAndRoots()
+		'While there are values to square
+		While (tbxMain.Text.Contains("²") Or tbxMain.Text.Contains("√"))
+
+			'Get pos of next sqaure and root
+			'InStr returns 0 when not found
+			Dim nextSquare = InStr(tbxMain.Text, "²")
+			Dim nextRoot = InStr(tbxMain.Text, "√")
+
+			'If there is a nextSquare and its before next root or there is no root (nextroot = 0 means no root found)
+			If (Not nextSquare = 0 AndAlso (nextSquare < nextRoot Or nextRoot = 0)) Then
+				CalculateSquare()
+
+				'Reversed, Keep all checks for redundancy
+			ElseIf (Not nextRoot = 0 AndAlso (nextSquare > nextRoot Or nextSquare = 0)) Then
+				CalculateRoot()
+			End If
+		End While
+	End Sub
+
+	Sub CalculateSquare()
+		'Prep next value to sqaure
+		'Remove all chars after it
+		Dim toSquare = tbxMain.Text.Substring(0, InStr(tbxMain.Text, "²") - 1)
+		'Get length
+		Dim len = toSquare.Length - 1
+		While ((IsNumeric(toSquare(len)) Or toSquare(len) = ",") And len >= 1)
+			len -= 1
+		End While
+		toSquare = toSquare.Substring(len + 1)
+		tbxMain.Text = tbxMain.Text.Replace(toSquare + "²", Math.Pow(Double.Parse(toSquare), 2))
+	End Sub
+
+	Sub CalculateRoot()
+		'Prep next value to root
+		'Remove all chars before it
+		Dim toRoot = tbxMain.Text.Substring(InStr(tbxMain.Text, "√"))
+		'Get length
+		Dim len = 0
+		While (len < toRoot.Length AndAlso (IsNumeric(toRoot(len)) Or toRoot(len) = "."))
+			len += 1
+		End While
+		toRoot = toRoot.Substring(0, len) 'Remove all chars after it
+
+		tbxMain.Text = tbxMain.Text.Replace("√" + toRoot, Math.Sqrt(Double.Parse(toRoot)))
+	End Sub
+
+	Sub CalculateMultiplicationAndDivison()
+		While (tbxMain.Text.Contains("X") Or tbxMain.Text.Contains("/"))
+			Dim nextMultiplication = InStr(tbxMain.Text, "X")
+			Dim nextDivision = InStr(tbxMain.Text, "/")
+
+			'If there is a nextSquare and its before next root or there is no root
+			If (Not nextMultiplication = 0 AndAlso (nextMultiplication < nextDivision Or nextDivision = 0)) Then
+				CalculateMultiplication()
+			ElseIf (Not nextDivision = 0 AndAlso (nextMultiplication > nextDivision Or nextMultiplication = 0)) Then 'Reversed, Keep all checks for redundancy
+				CalculateDivision()
+			End If
+		End While
+	End Sub
+
+	Sub CalculateMultiplication()
+		'Prep value 1
+		'Remove all after val1
+		Dim val1 = tbxMain.Text.Substring(0, InStr(tbxMain.Text, "X") - 1)
+		'Get val1 length
+		Dim len = val1.Length - 1
+		While ((Not IsNumeric(val1(len)) Or Not val1(len) = ",") And len >= 1)
+			len -= 1
+		End While
+		val1 = val1.Substring(len) 'Remove all chars before it
+
+		Dim val2 = tbxMain.Text.Substring(InStr(tbxMain.Text, "X"))
+		len = 0
+		While (len < val2.Length AndAlso (IsNumeric(val2(len)) Or val2(len) = "."))
+			len += 1
+		End While
+		val2 = val2.Substring(0, len) 'Remove all chars after it
+
+		tbxMain.Text = tbxMain.Text.Replace(val1 + "X" + val2, val1 * val2) '{val1}X{val2}... will be replaced by {val1 * val2}...
+
+	End Sub
+	Sub CalculateDivision()
+		'Prep value 1
+		'Remove all after val1
+		Dim val1 = tbxMain.Text.Substring(0, InStr(tbxMain.Text, "/") - 1)
+		'Get val1 length
+		Dim len = val1.Length - 1
+		While ((IsNumeric(val1(len)) Or val1(len) = ",") And len >= 1)
+			len -= 1
+		End While
+		val1 = val1.Substring(len) 'Remove all chars before it
+
+		Dim val2 = tbxMain.Text.Substring(InStr(tbxMain.Text, "/"))
+		len = 0
+		While (len < val2.Length AndAlso (IsNumeric(val2(len)) Or val2(len) = "."))
+			len += 1
+		End While
+		val2 = val2.Substring(0, len) 'Remove all chars after it
+
+		tbxMain.Text = tbxMain.Text.Replace(val1 + "/" + val2, val1 / val2)
+	End Sub
+
+	Sub CalculateAdditionAndSubtraction()
+		'Loop through all '+' and '-' in string
+		While (tbxMain.Text.Contains("+") Or tbxMain.Text.Contains("-"))
+
+			'Get pos of next plus and next minus
+			Dim nextAddition = InStr(tbxMain.Text, "+")
+			Dim nextSubtraction = InStr(tbxMain.Text, "-")
+
+			'If there is a addition and its before next subtraction or there is no subtraction
+			If (Not nextAddition = 0 AndAlso (nextAddition < nextSubtraction Or nextSubtraction = 0)) Then
+				CalculateAddition() 'Do addition next
+			ElseIf (Not nextSubtraction = 0 AndAlso (nextAddition > nextSubtraction Or nextAddition = 0)) Then 'Reversed, Keep all checks for redundancy
+				CalculateSubtraction() 'Do subtraction next
+			End If
+		End While
+	End Sub
+
+	Sub CalculateSubtraction()
+		'Prep value 1
+		'Remove all after val1
+		Dim val1 = tbxMain.Text.Substring(0, InStr(tbxMain.Text, "-") - 1)
+		'Get val1 length
+		Dim len = val1.Length - 1
+		While ((Not IsNumeric(val1(len)) Or Not val1(len) = ",") And len >= 1)
+			len -= 1
+		End While
+		val1 = val1.Substring(len) 'Remove all chars before it
+
+		Dim val2 = tbxMain.Text.Substring(InStr(tbxMain.Text, "-"))
+		len = 0
+		While (len < val2.Length AndAlso (IsNumeric(val2(len)) Or val2(len) = "."))
+			len += 1
+		End While
+		val2 = val2.Substring(0, len) 'Remove all chars after it
+
+		tbxMain.Text = tbxMain.Text.Replace(val1 + "-" + val2, Double.Parse(val1) - Double.Parse(val2)) '{val1}-{val2}... will be replaced by {val1 - val2}... Cast to be safe
+
+	End Sub
+	Sub CalculateAddition()
+		'Prep value 1
+		'Remove all after val1
+		Dim val1 = tbxMain.Text.Substring(0, InStr(tbxMain.Text, "+") - 1)
+		'Get val1 length
+		Dim len = val1.Length - 1
+		While ((Not IsNumeric(val1(len)) Or Not val1(len) = ",") And len >= 1)
+			len -= 1
+		End While
+		val1 = val1.Substring(len) 'Remove all chars before it
+
+		'Get val2 length
+		Dim val2 = tbxMain.Text.Substring(InStr(tbxMain.Text, "+"))
+		len = 0
+		While (len < val2.Length AndAlso (IsNumeric(val2(len)) Or val2(len) = "."))
+			len += 1
+		End While
+		val2 = val2.Substring(0, len) 'Remove all chars after it
+
+		tbxMain.Text = tbxMain.Text.Replace(val1 + "+" + val2, Double.Parse(val1) + Double.Parse(val2)) '+ works as string appender, so we need to cast to double
+	End Sub
+
+
+	Private Sub tbxMain_KeyPress(sender As Object, e As KeyPressEventArgs) Handles tbxMain.KeyPress
+		Dim valueLegal = False
+		Dim legalKeys = New Char() {"+", "-", "/", "*"}
+		'Handle comma, stop multiple commas being writen
+		If (e.KeyChar = "." AndAlso Not tbxMain.Text.Contains(".")) Then
+			valueLegal = True
+		End If
+		'Hanlde numbers
+		If (IsNumeric(e.KeyChar)) Then
+			valueLegal = True
+		End If
+
+		'Handle legal keys
+		If (legalKeys.Contains(e.KeyChar)) Then
+			valueLegal = True
+		End If
+
+		If (Not valueLegal) Then
+			e.Handled = True
+		End If
+
+	End Sub
+
+	Private Sub tbxMain_KeyDown(sender As Object, e As KeyEventArgs) Handles tbxMain.KeyDown
+		'TODO do the rest.
+		'If input is enter, caclulate
+		If (e.KeyCode = Keys.Enter) Then
+			Calculate()
+		End If
+		'If backspace and tbxMain.text isnt empty
+		If (e.KeyCode = Keys.Back AndAlso tbxMain.Text IsNot "") Then
+			tbxMain.Text = tbxMain.Text.Substring(0, tbxMain.Text.Length - 1)
+			tbxMain.Select(tbxMain.Text.Length, 0)
+		End If
+		'Stop moving of cursor instead of complicating backspacing
+		If (e.KeyCode = Keys.Right Or e.KeyCode = Keys.Left Or e.KeyCode = Keys.Up Or e.KeyCode = Keys.Down) Then
+			e.Handled = True
+		End If
+	End Sub
+
+	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+		tbxMain.Text = "√12X5/3²+5"
+	End Sub
+
+
+	'Rules:
+	'1: +, -, /, X, ²
+	'2: √,
+	'3: numbers
+	'4: comma
+	Sub buttonClick(buttonValue, rule)
+		If (rule = 1) Then
+			If (tbxMain.Text IsNot "" AndAlso (IsNumeric(tbxMain.Text(tbxMain.Text.Length - 1)) Or tbxMain.Text(tbxMain.Text.Length - 1) = "²")) Then
+				tbxMain.Text += buttonValue
+			End If
+		ElseIf (rule = 2) Then '√ prepends to value to root, need to check if legal before calculation instead
+			tbxMain.Text += buttonValue
+		ElseIf (rule = 3) Then
+			If (Not tbxMain.Text(tbxMain.Text.Length - 1) = "²") Then 'Cannot append number after ²
+				tbxMain.Text += buttonValue
+			End If
+		ElseIf (rule = 4) Then
+			If (IsNumeric(tbxMain.Text(tbxMain.Text.Length - 1))) Then 'Can only append to numbers
+				tbxMain.Text += buttonValue
+			End If
+		End If
+    End Sub
+
+	Private Sub btnSquare_Click(sender As Object, e As EventArgs) Handles btnSquare.Click
+		buttonClick("²", 1)
+	End Sub
+
+	Private Sub btnEqual_Click(sender As Object, e As EventArgs) Handles btnEqual.Click
+		Calculate()
+	End Sub
+
+	Private Sub btnSquareRoot_Click(sender As Object, e As EventArgs) Handles btnSquareRoot.Click
+		buttonClick("√", 2)
+	End Sub
+
+	Private Sub btnMultiply_Click(sender As Object, e As EventArgs) Handles btnMultiply.Click
+		buttonClick("X", 1)
+	End Sub
+
+	Private Sub btnDivision_Click(sender As Object, e As EventArgs) Handles btnDivision.Click
+		buttonClick("/", 1)
+	End Sub
+
+	Private Sub btnPlus_Click(sender As Object, e As EventArgs) Handles btnPlus.Click
+		buttonClick("+", 2)
+	End Sub
+
+	Private Sub btnNum0_Click(sender As Object, e As EventArgs) Handles btnNum0.Click
+		buttonClick("0", 3)
+	End Sub
+
+	Private Sub btnNum1_Click(sender As Object, e As EventArgs) Handles btnNum1.Click
+		buttonClick("1", 3)
+	End Sub
+
+	Private Sub btnComma_Click(sender As Object, e As EventArgs) Handles btnComma.Click
+		buttonClick(",", 4)
+	End Sub
+End Class
